b490a20090638e0d57983ddf5dd18c59a66e463a
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2023-09-15 16:48:27 +0530

    bug-fix: uploadTracks file fixed

--- src/utils/mutation/uploadTrack.js
+++ src/utils/mutation/uploadTrack.js
@@ -1,3 +1,40 @@
 import { gql} from "@apollo/client";
 import { client } from "../../main";
 
+const TRACK_INPUT = gql`
+  mutation Mutation($trackInput: TrackInput) {
+  uploadTrack(trackInput: $trackInput) {
+    _id
+    name
+    artistsID
+    artistsName
+    trackImage
+    trackUrl
+    genre
+    duration
+  }
+}
+`;
+
+export const getMutation = async (cid, artistId, duration, genre, name, trackImage) => {
+    try {
+        console.log("function called")
+        const { gqlloading, gqlerror, data } = await client.mutate({
+        mutation: TRACK_INPUT,
+        variables: {
+            "trackInput": {
+              "artistsId": artistId ? "65042d3d2ad56917e5fea179" : artistId,
+              "duration":duration? duration : 100,
+              "genre": genre ? genre : "Pop",
+              "name": name ? name : "temp name",
+              "trackImage": trackImage? trackImage : "https://i.imgur.com/fBuldry.png",
+              "trackUrl": cid ? cid : "sample track url",
+          },
+        },
+      });
+      console.log(data);
+    } 
+    catch(err) {
+      console.log(err);
+    }	
+};
