d7a400a6ad26fabf2a01810761735b5ec623a2b9
Muktanshu Mishra <muktanshumishra24@gmail.com>
2023-09-17 21:07:49 +0530

    merging dev upload and ragequit

---
+++ src/graphql/query/getArtistsByName.js
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+import { client } from "../../main";
+
+const GET_ARTIST_BY_NAME_QUERY = gql`
+  query Query($name: String) {
+    getArtistsByName(name: $name) {
+      _id
+      name
+      image
+      description
+      genre
+      tracksId
+      tracksName
+    }
+  }
+`;
+
+export const getArtistsByName = async (name) => {
+  try {
+    console.log("function called");
+    if (name != "" && name != null) {
+      const { gqlloading, gqlerror, data } = await client.query({
+        query: GET_ARTIST_BY_NAME_QUERY,
+        variables: {
+          name: name,
+        },
+      });
+      console.log(data["getArtistsByName"][0]);
+      return data["getArtistsByName"][0];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
