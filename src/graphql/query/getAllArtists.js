75a4648a5a17a6914a09eea242a6cc70c170ad2d
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2023-09-18 21:42:16 +0530

    getAllArtists Query added

---
+++ src/graphql/query/getAllArtists.js
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+import { client } from "../../main";
+
+const GET_ALL_ARTISTS = gql`
+query GetAllArtists {
+  getAllArtists {
+    _id
+    name
+    image
+    description
+    genre
+    tracksId
+    tracksName
+  }
+}
+`;
+
+export const getAllArtists = async (name) => {
+  try {
+    console.log("function called");
+    if (name != "" && name != null) {
+      const { gqlloading, gqlerror, data } = await client.query({
+        query: GET_ALL_ARTISTS,
+        variables: {
+          name: name,
+        },
+      });
+      console.log(data["getAllArtists"][0]);
+      return data["getAllArtists"][0];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
