9e3ffe7850c721a8bb9c7387c38748e2930f09cc
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2024-02-13 04:31:07 +0530

    feat: added search results column

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -6,16 +6,17 @@ import { gql } from '@apollo/client'
 import { client } from '../main'
 import { getArtistsByName } from "../graphql/query/getArtistsByName";
 import { getAllTracks } from '../graphql/query/getAllTracks';
+import { Link } from "react-router-dom";
 
 interface Props extends BoxProps {
 	value: string;
-	isLoading: boolean;
+	// isLoading: boolean;
 	onSearchChange: ( event: React.ChangeEvent<HTMLInputElement> ) => void;
 	resultRenderer: ( result: any ) => JSX.Element;
-	searchResult: ( resultData: any )=> JSX.Element;
+	// searchResult: ( resultData: any )=> JSX.Element;
 	onResultSelect: ( resultData: any ) => void;
 	resultListMaxHeight?: string;
-	searchResults?: any[];
+	// searchResults?: any[];
 	placeholder?: string;
 	input?: { iconPosition: 'left' | 'right'; };
 	noResultFoundText?: string;
@@ -25,35 +26,75 @@ export const Search = ( props: Props ) => {
 
 	const {
 		value,
-		isLoading,
+		// isLoading,
 		input,
 		onSearchChange,
 		resultRenderer,
 		onResultSelect,
 		resultListMaxHeight = '60vh',
 		placeholder = '',
-		searchResults = [],
 		noResultFoundText = 'No results found.',
 		...rest
 	} = props;
-
+	const [isLoading, setIsloading] = useState(false);
 	const { iconPosition = 'left' } = input || {};
-
+	const [searchResults, setSearchResults] = useState([{
+			"id": "",
+			"title": "",
+			"redirect": "",
+			"icon_url": ""
+		},
+	]);
 	const [ showResults, setShowResults ] = useState( false );
+	const [query, setQuery] = useState("");
 	
-	const [ artistName, setArtistName ] = useState("");
-	const [artistNotFound, setArtistNotFound] = useState(false);
-	const [artist, setArtist] = useState([]);
 	
+	useEffect(() => {
+		const delayDebounceFn = setTimeout(() => {
+			console.log("search query: ",query)
+			handleQuery(query);
+		}, 100)
+		return () => clearTimeout(delayDebounceFn)
+	  }, [query])
+
 	const handleQuery = async (value) => {
 		try {
-		  searchBarAutoComplete(value).then((data) => {
-			if (data == null || data == undefined) {
-			  setArtistNotFound(true);
-			} else {
-			  setArtistNotFound(false);
+			if(value.length>0){
+				setIsloading(true);
+				await searchBarAutoComplete(value).then((data) => {
+					searchResults.splice(0, searchResults.length)
+					console.log("search query 2: ",value)
+					if (data![0][0] !=null) {
+						setIsloading(false);
+						searchResults.splice(0, searchResults.length)
+						console.log("came to not null")
+						console.log(data![0])
+					  data![0].map(track => {
+						searchResults.push({
+							id: track["_id"],
+							title: track["name"],
+							redirect: "",
+							icon_url: ""
+						});
+					  })
+					}
+					else{
+						setIsloading(false);
+						searchResults.splice(0, searchResults.length)
+						console.log("came to null")
+						searchResults.push({
+							id: "not found",
+							title: "not found",
+							redirect: "",
+							icon_url: ""
+						})
+					}
+				  });
 			}
-		  });
+			else{
+				setIsloading(false);
+				searchResults.splice(0, searchResults.length)
+			}
 		} catch (err) {
 		  console.log(err);
 		}
@@ -62,6 +103,7 @@ export const Search = ( props: Props ) => {
 	const onBlur = () => {
 		setTimeout( () => {
 			setShowResults( false );
+			setIsloading(false);
 		}, 170 );
 	};
 
@@ -89,11 +131,14 @@ export const Search = ( props: Props ) => {
 					borderRadius="full"
 					placeholder={ placeholder }
 					value={ value }
-					onChange={(e) => {
-						setArtistName(e.target.value)
-						handleQuery(e.target.value).then((data) => {
-						//   setArtist(data);
-						});
+					onChange={async(e) => {
+						// setArtistName(e.target.value)
+						setIsloading(true);
+						setQuery(e.target.value);
+						// handleQuery(e.target.value);
+						// handleQuery(e.target.value).then((data) => {
+						// //   setArtist(data);
+						// });
 					  }}
 					onFocus={ () => setShowResults( true ) }
 					onBlur={ onBlur }
@@ -128,22 +173,40 @@ export const Search = ( props: Props ) => {
 						{
 							searchResults.length >= 0
 								? searchResults.map( result => (
-									<Box
-										key={ result.id || result._id || result.key }
-										borderBottom="1px solid rgba(34,36,38,.1)"
-										cursor="pointer"
-										_hover={ {
-											bgColor: '#f9fafb'
-										} }
-										onClick={ () => onResultSelect( result ) }
-									>
-										<Flex alignItems="center">
-											<Box p="0.8em" margin="0" color="black">
-												{ resultRenderer( result ) }
-												
-											</Box>
-										</Flex>
-									</Box>
+									<Link to={result.redirect}>
+										<Box
+											key={ result.id }
+											borderBottom="1px solid rgba(34,36,38,.1)"
+											cursor="pointer"
+											_hover={ {
+												bgColor: '#f9fafb'
+											} }
+											onClick={ () => {} }
+										>
+											<Flex alignItems="center">
+												<Box p="0.8em" margin="0" color="black"
+
+													bgColor="white"
+													maxHeight={ resultListMaxHeight }
+													overflowY="auto"
+													borderRadius="0.3em"
+													boxShadow="0 2px 4px 0 rgb(34 36 38 / 12%), 0 2px 10px 0 rgb(34 36 38 / 15%);"
+													sx={ {
+														"&::-webkit-scrollbar": {
+															display: 'none'
+														}
+													} }
+												>
+													{ 
+														
+													}
+														<Text fontWeight="medium" color="black">
+															{result.title}
+														</Text>
+												</Box>
+											</Flex>
+										</Box>
+									</Link>
 								) )
 								: value.length > 0 && !isLoading && (
 									<Box
