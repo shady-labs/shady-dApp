0c3baacb20469bbdcd7377002c53b3a1667a0e44
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2024-02-13 17:18:51 +0530

    chore: removed comments

--- src/graphql/query/searchBarAutoComplete.js
+++ src/graphql/query/searchBarAutoComplete.js
@@ -34,17 +34,6 @@ export const searchBarAutoComplete = async search => {
     console.log("data3: ", data[3][0])
     data[3].map(track => {
       console.log("[track]: ",track)
-      // data[0].add({
-      //   artistsID: track["artistsID"],
-      //   artistsName: track["artistsName"],
-      //   genre: track["genre"],
-      //   duration: track["duration"],
-      //   name: track["name"],
-      //   trackImage: track["trackImage"],
-      //   trackUrl: track["trackUrl"],
-      //   _typename: track["_typename"],
-      //   _id: track["_id"]
-      // })
       data[0] = [...data[0], track]
     })
     // console.log('data: ', data)
