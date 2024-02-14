bbeeac72dd71a5139738197b5c3be7aaa31b8377
Lakshay Garg <lakshaygarg.in@gmail.com>
2024-02-15 05:24:02 +0530

    Add Chakra UI StylesProvider and update SearchHeader styling

--- src/pages/ArtistePage.jsx
+++ src/pages/ArtistePage.jsx
@@ -4,9 +4,12 @@ import {
   Button,
   Divider,
   Flex,
+  Grid,
+  GridItem,
   Heading,
   Image,
   Text,
+  Spacer,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
@@ -18,6 +21,10 @@ import { playTrack, setTrackList } from "../redux/slices/playerSlice";
 import LoadingSkeleton from "../components/LoadingSkeleton";
 import { gql, useQuery } from "@apollo/client";
 import { searchTrackByArtistName } from "../graphql/query/searchBarAutoComplete";
+import Header from "../components/Header";
+import { Search } from "../components/SearchHeader";
+import ArtistModal from "../components/ArtistModal";
+import { LoginView } from "../components/signInButton";
 
 const ArtistePage = () => {
   const { artistName } = useParams();
@@ -90,84 +97,119 @@ const ArtistePage = () => {
   }
 
   return (
-    <Box minH="100vh" p={4} pb={32} pt={{ base: 16, md: 4 }}>
-      <Box pt={6}>
-        <Flex
-          maxW="full"
-          direction={{ base: "column", md: "row" }}
-          align="flex-start"
-          justify="flex-start"
-          gap={5}
+    <GridItem
+      sx={styles.background}
+      backgroundImage={data.getArtistsByName[0].image}
+    >
+      <Box >
+        <Box
+          minH="100vh"
+          p={4}
+          pb={32}
+          pt={{ base: 16, md: 4 }}
+          sx={styles.section}
         >
-          <Box minWidth="14rem" h="14rem">
-            <Image
-              src={data.getArtistsByName[0].image}
-              alt={data.getArtistsByName[0].name}
-              w="full"
-              h="full"
-              objectFit="cover"
-              rounded="3xl"
-            />
+          <Box maxW={"97%"}>
+            <Flex direction="row" justifyContent={"space-between"} pt={2}>
+              <Heading
+                as="h1"
+                fontSize={{ base: "3xl", md: "3xl" }}
+                color="accent.light"
+                fontWeight={200}
+                // pl={2}
+                
+              >
+                <Search />
+              </Heading>
+              <Box dir="Row" pb={4}>
+                <Flex>
+                  <ArtistModal />
+                  <Spacer display={{ base: "none", md: "block" }} mx={2} />
+                  {/* <DynamicWidget variant="modal" /> */}
+                  <LoginView />
+                </Flex>
+              </Box>
+            </Flex>
           </Box>
-          <Box>
-            <Text
-              fontSize={{ base: "sm", md: "md" }}
+          <Box pt={6}>
+            <Flex
               maxW="full"
-              color="zinc.300"
+              direction={{ base: "column", md: "row" }}
+              align="flex-start"
+              justify="flex-start"
+              gap={5}
             >
-              Introducing,
-            </Text>
-            <Heading
-              as="h1"
-              fontSize={{ base: "lg", md: "6xl" }}
-              color="accent.light"
-              mb={4}
-              fontWeight={600}
-            >
-              {data.getArtistsByName[0].name}
-            </Heading>
-            <Text
-              fontSize={{ base: "sm", md: "md" }}
-              maxW="full"
-              color="zinc.300"
-            >
-              {data.artist?.description}
-            </Text>
+              <Box minWidth="14rem" h="14rem">
+                <Image
+                  src={data.getArtistsByName[0].image}
+                  alt={data.getArtistsByName[0].name}
+                  w="full"
+                  h="full"
+                  objectFit="cover"
+                  rounded="3xl"
+                />
+              </Box>
+              <Box>
+                <Text
+                  fontSize={{ base: "sm", md: "lg" }}
+                  maxW="full"
+                  color="zinc.300"
+                >
+                  Introducing,
+                </Text>
+                <Heading
+                  as="h1"
+                  fontSize={{ base: "lg", md: "6xl" }}
+                  color="accent.light"
+                  mb={4}
+                  fontWeight={600}
+                >
+                  {data.getArtistsByName[0].name}
+                </Heading>
+                <Text
+                  fontSize={{ base: "lg", md: "lg" }}
+                  maxW="full"
+                  color="zinc.300"
+                  fontWeight={600}
+                >
+                  {data.getArtistsByName[0].description}
+                </Text>
+              </Box>
+            </Flex>
           </Box>
-        </Flex>
-        <Box mt={12}>
-          <Flex align="center" gap={6} mb={4}>
-            <Heading
-              as="h3"
-              fontSize={{ base: "lg", md: "xl" }}
-              fontWeight={600}
-            >
-              Songs
-            </Heading>
-            <Button
-              onClick={handlePlay}
-              display="inline-flex"
-              alignItems="center"
-              variant="unstyled"
-              bg="accent.light"
-              fontSize={{ base: "sm", md: "md" }}
-              color="white"
-              rounded="2rem"
-              py={1}
-              px={4}
-              leftIcon={<BsFillPlayFill size={20} />}
-            >
-              Play All
-            </Button>
-          </Flex>
-          <Divider w="full" h="1px" border="0" bg="zinc.600" mb={3} />
+          <Box mt={12}>
+            <Flex align="center" gap={6} mb={4}>
+              <Heading
+                as="h3"
+                fontSize={{ base: "lg", md: "xl" }}
+                fontWeight={600}
+              >
+                Songs
+              </Heading>
+              <Button
+                onClick={handlePlay}
+                display="inline-flex"
+                alignItems="center"
+                variant="unstyled"
+                bg="accent.light"
+                fontSize={{ base: "sm", md: "md" }}
+                color="white"
+                rounded="2rem"
+                py={1}
+                px={4}
+                leftIcon={<BsFillPlayFill size={20} />}
+              >
+                Play All
+              </Button>
+            </Flex>
 
-          <Flex direction="column" gap={4}>
-            {songs?.map((song) => (
-              
-              //  handlePlay={onSongPlay}
-              
-              <ArtisteSong
+            <Divider w="full" h="1px" border="0" bg="zinc.600" mb={3} />
+
+            <Flex direction="column" gap={4}>
+              {songs?.map((song) => (
+                //  handlePlay={onSongPlay}
+
+                <ArtisteSong
                   key={song._id}
                   song={{
                     _id: song._id,
@@ -184,13 +226,28 @@ const ArtistePage = () => {
                     type: "Song",
                     __v: 0,
                   }}
-                  />
-            ))}
-          </Flex>
+                />
+              ))}
+            </Flex>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </GridItem>
   );
+};
+
+const styles = {
+  background: {
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "35%",
+  },
+  section: {
+    backgroundColor: "transparent", //black --> #090b0c
+    backdropFilter: "saturate(60%) blur(40px)",
+  },
 };
 
 export default ArtistePage;
